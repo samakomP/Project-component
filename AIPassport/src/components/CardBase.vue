085eeea7cdace8eaa518afd7d0d876b1ca024248
Prototean <teanchai_ch@cmu.ac.th>
2026-08-18 22:49:48 +0700

    Add page Landing page for User

---
+++ AIPassport/src/components/CardBase.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full max-w-7xl mx-auto bg-gray-100 rounded-[2.5rem] p-8 md:p-12 shadow-sm h-fit">
+    <header v-if="$slots.header" class="mb-8">
+        <slot name="header"></slot>
+      </header>
+      
+      <main>
+        <slot name="default"></slot>
+      </main>
+  </div>
+</template>
