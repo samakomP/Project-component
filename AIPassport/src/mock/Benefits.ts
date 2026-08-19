84ea26a00f2a476e14b31724dd5fd8f56df03a45
BlackShima <blackshima02@gmail.com>
2026-08-19 20:56:15 +0700

    create benefits.ts and put information

---
+++ AIPassport/src/mock/Benefits.ts
@@ -0,0 +1,30 @@
+export const Benefits = [
+    {
+      "benefits_ID": 1,
+      "level_ID": 1,
+      "benefitName": "Starter Badge",
+      "description": "Receive a badge for completing Level 1.",
+      "badgeIcon": "/images/badges/starter.png"
+    },
+    {
+      "benefits_ID": 2,
+      "level_ID": 2,
+      "benefitName": "Intermediate Badge",
+      "description": "Unlock the intermediate learner badge.",
+      "badgeIcon": "/images/badges/intermediate.png"
+    },
+    {
+      "benefits_ID": 3,
+      "level_ID": 3,
+      "benefitName": "Advanced Badge",
+      "description": "Unlock the advanced learner badge.",
+      "badgeIcon": "/images/badges/advanced.png"
+    },
+    {
+      "benefits_ID": 4,
+      "level_ID": 4,
+      "benefitName": "Expert Badge",
+      "description": "Unlock the expert learner badge.",
+      "badgeIcon": "/images/badges/expert.png"
+    }
+]
