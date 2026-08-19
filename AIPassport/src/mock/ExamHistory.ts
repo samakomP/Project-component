4d062834573c0b5e0bf61f6e995d8f1e2553a8c2
BlackShima <blackshima02@gmail.com>
2026-08-19 20:58:32 +0700

    create examhistory.ts and put information

---
+++ AIPassport/src/mock/ExamHistory.ts
@@ -0,0 +1,50 @@
+export const ExamHistory = [
+    {
+      "exam_ID": 1,
+      "users_ID": 2,
+      "level_ID": 1,
+      "score": 80,
+      "result": "pass",
+      "timestamp": "2026-08-01T09:30:00+07:00"
+    },
+    {
+      "exam_ID": 2,
+      "users_ID": 2,
+      "level_ID": 2,
+      "score": 65,
+      "result": "fail",
+      "timestamp": "2026-08-03T10:15:00+07:00"
+    },
+    {
+      "exam_ID": 3,
+      "users_ID": 3,
+      "level_ID": 2,
+      "score": 85,
+      "result": "pass",
+      "timestamp": "2026-08-02T13:00:00+07:00"
+    },
+    {
+      "exam_ID": 4,
+      "users_ID": 3,
+      "level_ID": 3,
+      "score": 70,
+      "result": "fail",
+      "timestamp": "2026-08-05T14:20:00+07:00"
+    },
+    {
+      "exam_ID": 5,
+      "users_ID": 5,
+      "level_ID": 2,
+      "score": 75,
+      "result": "pass",
+      "timestamp": "2026-08-06T11:10:00+07:00"
+    },
+    {
+      "exam_ID": 6,
+      "users_ID": 4,
+      "level_ID": 3,
+      "score": 82,
+      "result": "pass",
+      "timestamp": "2026-08-07T15:45:00+07:00"
+    }
+]
