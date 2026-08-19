ad939f738293994a7b4e4900658275d6e1b8fefc
BlackShima <blackshima02@gmail.com>
2026-08-19 20:53:28 +0700

    create questionsOptions.ts and put information

---
+++ AIPassport/src/mock/questionsOptions.ts
@@ -0,0 +1,202 @@
+export const questionsOptions = [
+    {
+      "option_ID": 1,
+      "question_ID": 1,
+      "optionText": "Hyper Trainer Markup Language"
+    },
+    {
+      "option_ID": 2,
+      "question_ID": 1,
+      "optionText": "HyperText Markup Language"
+    },
+    {
+      "option_ID": 3,
+      "question_ID": 1,
+      "optionText": "HighText Machine Language"
+    },
+    {
+      "option_ID": 4,
+      "question_ID": 1,
+      "optionText": "Hyperlink Text Management Language"
+    },
+    {
+      "option_ID": 5,
+      "question_ID": 2,
+      "optionText": "HTML"
+    },
+    {
+      "option_ID": 6,
+      "question_ID": 2,
+      "optionText": "Java"
+    },
+    {
+      "option_ID": 7,
+      "question_ID": 2,
+      "optionText": "CSS"
+    },
+    {
+      "option_ID": 8,
+      "question_ID": 2,
+      "optionText": "SQL"
+    },
+    {
+      "option_ID": 9,
+      "question_ID": 3,
+      "optionText": " // "
+    },
+    {
+      "option_ID": 10,
+      "question_ID": 3,
+      "optionText": " ## "
+    },
+    {
+      "option_ID": 11,
+      "question_ID": 3,
+      "optionText": " <!-- --> "
+    },
+    {
+      "option_ID": 12,
+      "question_ID": 3,
+      "optionText": " ** "
+    },
+    {
+      "option_ID": 13,
+      "question_ID": 4,
+      "optionText": "class"
+    },
+    {
+      "option_ID": 14,
+      "question_ID": 4,
+      "optionText": "new"
+    },
+    {
+      "option_ID": 15,
+      "question_ID": 4,
+      "optionText": "object"
+    },
+    {
+      "option_ID": 16,
+      "question_ID": 4,
+      "optionText": "define"
+    },
+    {
+      "option_ID": 17,
+      "question_ID": 5,
+      "optionText": "Stack"
+    },
+    {
+      "option_ID": 18,
+      "question_ID": 5,
+      "optionText": "Tree"
+    },
+    {
+      "option_ID": 19,
+      "question_ID": 5,
+      "optionText": "Graph"
+    },
+    {
+      "option_ID": 20,
+      "question_ID": 5,
+      "optionText": "Queue"
+    },
+    {
+      "option_ID": 21,
+      "question_ID": 6,
+      "optionText": "POST"
+    },
+    {
+      "option_ID": 22,
+      "question_ID": 6,
+      "optionText": "GET"
+    },
+    {
+      "option_ID": 23,
+      "question_ID": 6,
+      "optionText": "DELETE"
+    },
+    {
+      "option_ID": 24,
+      "question_ID": 6,
+      "optionText": "PATCH"
+    },
+    {
+      "option_ID": 25,
+      "question_ID": 7,
+      "optionText": "O(1)"
+    },
+    {
+      "option_ID": 26,
+      "question_ID": 7,
+      "optionText": "O(n)"
+    },
+    {
+      "option_ID": 27,
+      "question_ID": 7,
+      "optionText": "O(log n)"
+    },
+    {
+      "option_ID": 28,
+      "question_ID": 7,
+      "optionText": "O(n²)"
+    },
+    {
+      "option_ID": 29,
+      "question_ID": 8,
+      "optionText": "Inheritance"
+    },
+    {
+      "option_ID": 30,
+      "question_ID": 8,
+      "optionText": "Encapsulation"
+    },
+    {
+      "option_ID": 31,
+      "question_ID": 8,
+      "optionText": "Polymorphism"
+    },
+    {
+      "option_ID": 32,
+      "question_ID": 8,
+      "optionText": "Abstraction"
+    },
+    {
+      "option_ID": 33,
+      "question_ID": 9,
+      "optionText": "Inheritance"
+    },
+    {
+      "option_ID": 34,
+      "question_ID": 9,
+      "optionText": "Compilation"
+    },
+    {
+      "option_ID": 35,
+      "question_ID": 9,
+      "optionText": "Iteration"
+    },
+    {
+      "option_ID": 36,
+      "question_ID": 9,
+      "optionText": "Encapsulation"
+    },
+    {
+      "option_ID": 37,
+      "question_ID": 10,
+      "optionText": "Version control"
+    },
+    {
+      "option_ID": 38,
+      "question_ID": 10,
+      "optionText": "Continuous Integration"
+    },
+    {
+      "option_ID": 39,
+      "question_ID": 10,
+      "optionText": "Manual testing"
+    },
+    {
+      "option_ID": 40,
+      "question_ID": 10,
+      "optionText": "Code formatting"
+    }
+]
