807baeebe41f12424e8e84eba698aa11109faa52
Prototean <teanchai_ch@cmu.ac.th>
2026-08-20 17:58:29 +0700

    StatTile for using Stat for two stat in PrepExam

--- AIPassport/src/components/StatTile.vue
+++ AIPassport/src/components/StatTile.vue
@@ -7,8 +7,8 @@ defineProps<{
 </script>
 
 <template>
-  <CardBase class="!bg-[#dcdcdc] !rounded-[40px] !shadow-none p-6 flex flex-col items-center justify-center aspect-square text-center">
-    <span class="text-xl font-bold mb-3"><slot/></span><br/>
-    <span class="text-4xl font-bold">{{ value }}</span>
+  <CardBase class="!bg-[#dcdcdc] !rounded-[40px] !shadow-none p-6 flex flex-col static items-center justify-center aspect-square text-center">
+    <span class="inline-block items-center justify-center text-3xl font-bold mb-3"><slot/></span><br/>
+    <span class="items-center justify-center text-4xl font-bold">{{ value }}</span>
   </CardBase>
 </template>
